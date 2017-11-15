3101a7a2c63640d9be18bafa743617a14910b2ac
Luke Davies <lukedavies@Lukes-MacBook.local>
2017-11-15 17:24:11 +0000

    first commit

---
+++ app/api-app.php
@@ -0,0 +1,44 @@
+<?php
+
+require_once('bootstrap.php');
+
+$headers = [
+    'user-key' => '0d5481df328381f738511ecf6ecd9ec5',
+    'Accept' => 'application/json'
+];
+
+$fileName = 'games.csv';
+
+$client = new GuzzleHttp\Client([
+    'headers' => $headers,
+]);
+
+$results = $client->get('https://api-2445582011268.apicast.io/games/?fields=*&limit=1');
+
+$data = file_get_contents($fileName);
+
+$data .= $results->getBody();
+
+file_put_contents($fileName, $data);
+
+$getData = file_get_contents($fileName);
+$dataArray = json_decode($getData, true);
+
+$fp = fopen($fileName, "w");
+foreach ($dataArray as $fields) {
+    fputcsv($fp, array_flatten($fields));
+}
+fclose($fp);
+
+
+function array_flatten ($nonFlat) {
+    $flat = array();
+    foreach (new RecursiveIteratorIterator(
+            new RecursiveArrayIterator($nonFlat)) as $k=>$v) {
+        $flat[$k] = $v;
+    }
+    return $flat;
+}
+
+
+echo 'done..';
